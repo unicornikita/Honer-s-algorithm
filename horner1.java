d03ac2fd972b24f89f3537e2896fada7fabe5adb
unicornikita <nikitakapusin@gmail.com>
2018-10-11 20:47:33 +0200

    updated code

--- horner1.java
+++ horner1.java
@@ -7,40 +7,62 @@ public class horner1
     Scanner sc = new Scanner(System.in);
     System.out.println("Vnesi stopnjo polinoma:");
     int NajvecjaStopnja = sc.nextInt();
+    int stopnje[][] = new int[1][NajvecjaStopnja+1];
+    int koeficienti[][] = new int[1][NajvecjaStopnja+1];
     int sestevek = NajvecjaStopnja;
-    int stopnje[][] = new int[100][NajvecjaStopnja+1];
-    stopnje[0][0] = NajvecjaStopnja;
-    for (int i = 1; i <= NajvecjaStopnja; i++)
+    for (int i = 0; i <= NajvecjaStopnja+1; i++)
     {
+        if (i == 0)
+        {
+            stopnje[0][i] = NajvecjaStopnja;
+        }
+        else if(i!=0)
+     {
         stopnje[0][i] = NajvecjaStopnja-1;
-        NajvecjaStopnja--;
+    }
+
+    NajvecjaStopnja--;
     }
     
-    for (int j=0; j< stopnje.length; j++)
+    /*int i=0;
+    while(i <= NajvecjaStopnja)
     {
-          System.out.println("Kater koeficient je pri stopnji " + stopnje [0][j]);
-          stopnje[1][j] = sc.nextInt();  
+        if (i == 0)
+        {
+            stopnje[0][i] = NajvecjaStopnja;
+        }
+        else if(i!=0)
+     {
+        stopnje[0][i] = NajvecjaStopnja-1;
+    }
+
+    NajvecjaStopnja--;
+    i++;
+    }
+    */
+    for (int j=0; j <= stopnje.length; j++)
+    {
+          System.out.println("Kater koeficient je pri stopnji " + stopnje[0][j]);
+          koeficienti[0][j] = sc.nextInt();  
           /*if(stopnje[0][j] == 0)
             break; */        
-    int zmnozek = 0;
-    int k = 0;
-    do
-    {
-        sestevek = stopnje[1][k] + zmnozek;
-        zmnozek = sestevek;
+    }
 
-    if (k == stopnje.length && sestevek == 0)
+    int zmnozek = 0;
+
+   for (int k = 0; k < stopnje.length; k++)
+    {
+        sestevek = koeficienti[0][k] + zmnozek;
+        zmnozek = sestevek;
+    }
+    if (sestevek == 0)
         {
             System.out.println("Ena je nicla polinoma");
-            break;
         }
         
-    else if (k == stopnje.length && sestevek != 0)
+    else if (sestevek != 0)
             System.out.println("Ena ni nicla polinoma");
-    
-    k++;
-    } while(sestevek != 0);
 
-}
-    }
+    } 
+    
 }
