31d35dd51c2dd1c35652f94151acdb5132505a0d
unicornikita <nikitakapusin@gmail.com>
2018-10-11 22:07:26 +0200

    final horner algorithm

--- horner1.java
+++ horner1.java
@@ -1,68 +1,46 @@
 import java.util.Scanner;
-import java.util.ArrayList; 
-import java.util.List;
 public class horner1
 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
+    // Asking the user to input the highest power of the polynom and saving it in NajvecjaStopnja
     System.out.println("Vnesi stopnjo polinoma:");
     int NajvecjaStopnja = sc.nextInt();
-    int stopnje[][] = new int[1][NajvecjaStopnja+1];
-    int koeficienti[][] = new int[1][NajvecjaStopnja+1];
-    int sestevek = NajvecjaStopnja;
-    for (int i = 0; i <= NajvecjaStopnja+1; i++)
+
+    //declaring an array of all the powers the polynom has left in stopnje[] and the coefficient in koeficienti[]
+    int stopnje[] = new int[NajvecjaStopnja+1];
+    int koeficienti[] = new int[NajvecjaStopnja+1];
+
+    //filling the stopnje[] array with powers between 0 and NajvecjaStopnja
+    for(int i = 0; i < NajvecjaStopnja+1; i++)
+        stopnje[i] = NajvecjaStopnja-i;
+
+    // filling the koeficienti[] array with the coefficients of a certain power
+    for (int j=0; j < stopnje.length; j++)
     {
-        if (i == 0)
-        {
-            stopnje[0][i] = NajvecjaStopnja;
-        }
-        else if(i!=0)
-     {
-        stopnje[0][i] = NajvecjaStopnja-1;
+          System.out.println("Kater koeficient je pri stopnji " + stopnje[j]);
+          koeficienti[j] = sc.nextInt();         
     }
 
-    NajvecjaStopnja--;
-    }
-    
-    /*int i=0;
-    while(i <= NajvecjaStopnja)
-    {
-        if (i == 0)
-        {
-            stopnje[0][i] = NajvecjaStopnja;
-        }
-        else if(i!=0)
-     {
-        stopnje[0][i] = NajvecjaStopnja-1;
-    }
+    // the sum we will need later to check if number 1 is one of the zeros of the polynom
+    int sestevek = koeficienti[0];
 
-    NajvecjaStopnja--;
-    i++;
-    }
-    */
-    for (int j=0; j <= stopnje.length; j++)
-    {
-          System.out.println("Kater koeficient je pri stopnji " + stopnje[0][j]);
-          koeficienti[0][j] = sc.nextInt();  
-          /*if(stopnje[0][j] == 0)
-            break; */        
-    }
-
+    //  zmnozek will save the result of sums you multiply with 1
     int zmnozek = 0;
 
+    // sestevek is saving the sum of coefficients on index k and the sum of the first multiplication, then the sum of coefficients is saved in zmnozek
    for (int k = 0; k < stopnje.length; k++)
     {
-        sestevek = koeficienti[0][k] + zmnozek;
+        sestevek = koeficienti[k] + zmnozek;
         zmnozek = sestevek;
     }
+    // the algorithm lets you know if 1 is a zero of the polynom
     if (sestevek == 0)
-        {
             System.out.println("Ena je nicla polinoma");
-        }
         
     else if (sestevek != 0)
-            System.out.println("Ena ni nicla polinoma");
+        System.out.println("Ena ni nicla polinoma");
 
-    } 
-    
+    }
 }
